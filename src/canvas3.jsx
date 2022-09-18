4bb8621a684026ba4347037603426268c1ea2fbe
amirhossein esfahanian <amire410@gmail.com>
2022-09-18 19:59:53 +0430

    drawing mouse up need to be fixed

--- src/canvas3.jsx
+++ src/canvas3.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
 import svgForm from "./assets/grid.svg";
+import plusSvg from "./assets/plus.svg";
+import closeSvg from "./assets/close.svg";
+import checkSvg from "./assets/check.svg";
+import resizeSvg from "./assets/resize.svg";
 
 export default function Canvas3() {
   let z = undefined;
   let form = undefined;
   let svg = undefined;
+  let elements = undefined;
   let dragItem = undefined;
   let counter = 0;
   let dragAction = "move";
@@ -14,7 +19,85 @@ export default function Canvas3() {
   const [w, setW] = useState(window.innerWidth);
   const [isZooming, setZoom] = useState(true);
   const [h, setH] = useState(window.innerHeight);
+  function drawDone() {
+    var mu = d3.mouse(this);
 
+    newSelect
+      .append("image")
+      .attr("xlink:href", closeSvg)
+      .style("width", 25 + "px")
+      .style("background-color", "red")
+      .style("height", 25 + "px")
+      .classed("item" + counter, "select")
+
+      .attr("x", mu[0])
+      .attr("y", m[1])
+      .on("click", function () {
+        //id and class number is not same so have to select separately
+        d3.select(idSelector).remove();
+        d3.selectAll(classSelector).remove();
+      });
+
+    newSelect
+      .append("image")
+      .attr("xlink:href", checkSvg)
+      .style("width", 25 + "px")
+      .on("click", function () {
+        d3.select(idSelector).classed("item", "select");
+        d3.select(idSelector).on("mousedown", null);
+        d3.select(idSelector).on("mouseup", null);
+        alert("try to save");
+        d3.selectAll(classSelector).style("display", "none");
+        const parent = d3.select(parentSelector);
+        const parentPosition = parent.node().getBBox();
+        parent
+          .append("text")
+          .style("fill", "black")
+          .style("font-size", "14px")
+          .attr("dy", ".35em")
+          .attr("x", parentPosition.x + 3)
+          //set text center vertically
+          .attr("y", parentPosition.y + parentPosition.height / 2)
+          .style("style", "label")
+          .text("Helllo");
+
+        newSelect
+
+          .append("circle")
+          .attr("r", 5)
+          .attr("id", "resizeHandle" + counter)
+          .classed("item" + counter, "select")
+          .attr("cx", mu[0])
+          .attr("cy", mu[1])
+
+          .on("mouseover", function () {
+            dragAction = "resize";
+          })
+          .on("mouseleave", function () {});
+      })
+      .classed("item" + counter, "select")
+
+      .style("height", 25 + "px")
+      .attr("x", mu[0])
+      .attr("y", mu[1]);
+
+    var drag_handler = d3
+      .drag()
+      .on("drag", () => drag(parentSelector, idSelector, resizeSelector));
+    var resize_handler = d3.drag().on("drag", function (d) {
+      console.log(resizeSelector);
+    });
+
+    //choose drag what to start dragging
+    resize_handler(
+      d3.select("#my-svg").select(parentSelector).select(resizeSelector)
+    );
+    drag_handler(d3.select("#my-svg").select(parentSelector));
+    const field = d3.select("#my-svg").select(idSelector);
+    const parent = d3.select("#my-svg").select("#front");
+    field.on("mousemove", null);
+    parent.on("mousemove", null);
+  }
   function mousemove(d) {
     let idSelector = null;
     let parentSelector = null;
@@ -116,8 +199,144 @@ export default function Canvas3() {
               .attr("width", "100%")
               .attr("height", "100%")
               .attr("fill", "transparent")
-              .attr("id", "zoomCover");
+              .attr("id", "zoomCover")
+              .on("mousemove", mousemove)
+              .on("mouseup", function () {
+                alert("Up1");
 
+                // const parent = d3.select("#my-svg").select("#front");
+                // parent.on("mousemove", null);
+              })
+              //parent mouse down start drawing field
+              .on("mousedown", function () {
+                dragAction = "move";
+                d3.select("#my-svg")
+                  .select("#root-g")
+                  .selectAll("rect")
+                  .classed("resizing", false);
+                var m = d3.mouse(this);
+                const idSelector = `#select${counter}`;
+                const parentSelector = `#selectParent${counter}`;
+                const classSelector = `.item${counter + 1}`;
+                const resizeSelector = `#resizeHandle${counter + 1}`;
+
+                //append field
+                const newSelect = elements
+                  .append("svg")
+                  .attr("id", "selectParent" + counter);
+
+                newSelect
+                  .append("rect")
+                  .attr("x", m[0])
+                  .attr("y", m[1])
+                  .attr("height", 0)
+                  .attr("width", 0)
+                  .attr("fill", "green")
+                  .attr("border", 1)
+                  .attr("href", "#pointer")
+                  // .classed("item", "select")
+                  .attr("id", "select" + counter)
+                  .on("mousemove", mousemove)
+
+                  .on("mousedown", function () {
+                    const field = d3.select("#my-svg").select(idSelector);
+
+                    //   field.on("mousemove", mousemove);
+                  })
+                  .on("mouseup", function () {
+                    var mu = d3.mouse(this);
+
+                    newSelect
+                      .append("image")
+                      .attr("xlink:href", closeSvg)
+                      .style("width", 25 + "px")
+                      .style("background-color", "red")
+                      .style("height", 25 + "px")
+                      .classed("item" + counter, "select")
+
+                      .attr("x", mu[0])
+                      .attr("y", m[1])
+                      .on("click", function () {
+                        //id and class number is not same so have to select separately
+                        d3.select(idSelector).remove();
+                        d3.selectAll(classSelector).remove();
+                      });
+
+                    newSelect
+                      .append("image")
+                      .attr("xlink:href", checkSvg)
+                      .style("width", 25 + "px")
+                      .on("click", function () {
+                        d3.select(idSelector).classed("item", "select");
+                        d3.select(idSelector).on("mousedown", null);
+                        d3.select(idSelector).on("mouseup", null);
+                        alert("try to save");
+                        d3.selectAll(classSelector).style("display", "none");
+                        const parent = d3.select(parentSelector);
+                        const parentPosition = parent.node().getBBox();
+                        parent
+                          .append("text")
+                          .style("fill", "black")
+                          .style("font-size", "14px")
+                          .attr("dy", ".35em")
+                          .attr("x", parentPosition.x + 3)
+                          //set text center vertically
+                          .attr(
+                            "y",
+                            parentPosition.y + parentPosition.height / 2
+                          )
+                          .style("style", "label")
+                          .text("Helllo");
+
+                        newSelect
+
+                          .append("circle")
+                          .attr("r", 5)
+                          .attr("id", "resizeHandle" + counter)
+                          .classed("item" + counter, "select")
+                          .attr("cx", mu[0])
+                          .attr("cy", mu[1])
+
+                          .on("mouseover", function () {
+                            dragAction = "resize";
+                          })
+                          .on("mouseleave", function () {});
+                      })
+                      .classed("item" + counter, "select")
+
+                      .style("height", 25 + "px")
+                      .attr("x", mu[0])
+                      .attr("y", mu[1]);
+
+                    var drag_handler = d3
+                      .drag()
+                      .on("drag", () =>
+                        drag(parentSelector, idSelector, resizeSelector)
+                      );
+                    var resize_handler = d3.drag().on("drag", function (d) {
+                      console.log(resizeSelector);
+                    });
+
+                    //choose drag what to start dragging
+                    resize_handler(
+                      d3
+                        .select("#my-svg")
+                        .select(parentSelector)
+                        .select(resizeSelector)
+                    );
+                    drag_handler(d3.select("#my-svg").select(parentSelector));
+                    const field = d3.select("#my-svg").select(idSelector);
+                    const parent = d3.select("#my-svg").select("#front");
+                    field.on("mousemove", null);
+                    parent.on("mousemove", null);
+                  });
+
+                const field = d3.select("#my-svg").select(idSelector);
+                const parent = d3.select("#my-svg").select("#front");
+                parent.on("mousemove", mousemove);
+                field.on("mousemove", mousemove);
+                counter++;
+              });
             setZoom(false);
           } else {
             d3.select("#my-svg").select("#zoomCover").remove();
@@ -149,144 +368,13 @@ export default function Canvas3() {
             .on("click", function () {
               console.log(d3.mouse(this));
             });
-          const elements = newG.append("g").attr("id", "elements-g");
+          elements = newG.append("g").attr("id", "elements-g");
 
           elements
             .append("rect")
             .attr("width", "50%")
             .attr("height", "90%")
-            .on("mousemove", mousemove)
-            .on("mouseup", function () {
-              const parent = d3.select("#my-svg").select("#front");
-              parent.on("mousemove", null);
-            })
-            //parent mouse down start drawing field
-            .on("mousedown", function () {
-              dragAction = "move";
-              d3.select("#my-svg")
-                .select("#root-g")
-                .selectAll("rect")
-                .classed("resizing", false);
-              var m = d3.mouse(this);
-              const idSelector = `#select${counter}`;
-              const parentSelector = `#selectParent${counter}`;
-              const classSelector = `.item${counter + 1}`;
-              const resizeSelector = `#resizeHandle${counter + 1}`;
 
-              //append field
-              const newSelect = svg
-                .append("svg")
-                .attr("id", "selectParent" + counter);
-
-              newSelect
-                .append("rect")
-                .attr("x", m[0])
-                .attr("y", m[1])
-                .attr("height", 0)
-                .attr("width", 0)
-                .attr("fill", "green")
-                .attr("border", 1)
-                .attr("href", "#pointer")
-                // .classed("item", "select")
-                .attr("id", "select" + counter)
-                .on("mousemove", mousemove)
-
-                .on("mousedown", function () {
-                  const field = d3.select("#my-svg").select(idSelector);
-
-                  //   field.on("mousemove", mousemove);
-                })
-                .on("mouseup", function () {
-                  var mu = d3.mouse(this);
-
-                  newSelect
-                    .append("image")
-                    .attr("xlink:href", closeSvg)
-                    .style("width", 25 + "px")
-                    .style("background-color", "red")
-                    .style("height", 25 + "px")
-                    .classed("item" + counter, "select")
-
-                    .attr("x", mu[0])
-                    .attr("y", m[1])
-                    .on("click", function () {
-                      //id and class number is not same so have to select separately
-                      d3.select(idSelector).remove();
-                      d3.selectAll(classSelector).remove();
-                    });
-
-                  newSelect
-                    .append("image")
-                    .attr("xlink:href", checkSvg)
-                    .style("width", 25 + "px")
-                    .on("click", function () {
-                      d3.select(idSelector).classed("item", "select");
-                      d3.select(idSelector).on("mousedown", null);
-                      d3.select(idSelector).on("mouseup", null);
-                      alert("try to save");
-                      d3.selectAll(classSelector).style("display", "none");
-                      const parent = d3.select(parentSelector);
-                      const parentPosition = parent.node().getBBox();
-                      parent
-                        .append("text")
-                        .style("fill", "black")
-                        .style("font-size", "14px")
-                        .attr("dy", ".35em")
-                        .attr("x", parentPosition.x + 3)
-                        //set text center vertically
-                        .attr("y", parentPosition.y + parentPosition.height / 2)
-                        .style("style", "label")
-                        .text("Helllo");
-
-                      newSelect
-
-                        .append("circle")
-                        .attr("r", 5)
-                        .attr("id", "resizeHandle" + counter)
-                        .classed("item" + counter, "select")
-                        .attr("cx", mu[0])
-                        .attr("cy", mu[1])
-
-                        .on("mouseover", function () {
-                          dragAction = "resize";
-                        })
-                        .on("mouseleave", function () {});
-                    })
-                    .classed("item" + counter, "select")
-
-                    .style("height", 25 + "px")
-                    .attr("x", mu[0])
-                    .attr("y", mu[1]);
-
-                  var drag_handler = d3
-                    .drag()
-                    .on("drag", () =>
-                      drag(parentSelector, idSelector, resizeSelector)
-                    );
-                  var resize_handler = d3.drag().on("drag", function (d) {
-                    console.log(resizeSelector);
-                  });
-
-                  //choose drag what to start dragging
-                  resize_handler(
-                    d3
-                      .select("#my-svg")
-                      .select(parentSelector)
-                      .select(resizeSelector)
-                  );
-                  drag_handler(d3.select("#my-svg").select(parentSelector));
-                  const field = d3.select("#my-svg").select(idSelector);
-                  const parent = d3.select("#my-svg").select("#front");
-                  field.on("mousemove", null);
-                  parent.on("mousemove", null);
-                });
-
-              const field = d3.select("#my-svg").select(idSelector);
-              const parent = d3.select("#my-svg").select("#front");
-              parent.on("mousemove", mousemove);
-              field.on("mousemove", mousemove);
-              counter++;
-            })
             .attr("fill", "red")
             .attr("id", "drawer");
 
